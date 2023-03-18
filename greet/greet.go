78990024f4c14a00997ec007709b0b27f060366d
Luis Ochoa <occhoa90@gmail.com>
2023-03-18 12:50:02 -0700

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "🙋"
+
+// GreetEnglish retorna saludo en ingles
+func English() string {
+	return "hi " + emoji
+}
+
+// Italian retorna saludo en italiano
+func Italian() string {
+	return "Ciao " + emoji
+}
